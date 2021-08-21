963ac3ba168cefd49161e61f1558602d4f5c46bc
Sanjana Rao <sanjanagrao99@gmail.com>
2021-08-21 10:42:15 +0530

    Added: Welcome Page

---
+++ AddressBookMain.java
@@ -0,0 +1,11 @@
+package bridgelabzPractice;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		//Display Message.
+		System.out.println("Welcome to Address Book Program");
+
+	}
+
+}
