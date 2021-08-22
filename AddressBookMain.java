b55e97aec4d60d39dca81f13ed538e946a735043
Sanjana Rao <sanjanagrao99@gmail.com>
2021-08-22 18:12:59 +0530

    Added: Address Book System UC3

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class AddressBookMain {
 	
+	public static  int n=0;
+	
 	//Class members - UC1
 		private String firstName;
 		private String lastName;
@@ -12,9 +14,10 @@ public class AddressBookMain {
 		private String state;
 		private String pin;
 		private String phNumber;
+		private String email;
 		
 		//Constructor -UC1
-		public AddressBookMain(String firstName, String lastName, String address, String city, String state, String pin, String phNumber)
+		public AddressBookMain(String firstName, String lastName, String address, String city, String state, String pin, String phNumber, String email)
 		{
 			this.firstName = firstName;
 			this.lastName = lastName;
@@ -23,43 +26,130 @@ public class AddressBookMain {
 			this.state = state;
 			this.pin = pin;
 			this.phNumber = phNumber;
+			this.email = email;
 		}
+		
+		
+		public void Display()
+		{	
+			//Displaying the details
+			System.out.println("Name: " + firstName + " " + lastName);
+			System.out.println("Address: " +address + ", " + city + ", "+ state + " - " + pin);
+			System.out.println("Contact Number: " +phNumber);
+			System.out.println("Email ID: " + email + "\n");
+		}
+		
+		//Function to edit existing contacts -UC3
+		public void editContact(String firstName, String lastName, String address, String city, String state, String pin, String phNumber, String email)
+		{
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.address = address;
+			this.city = city;
+			this.state = state;
+			this.pin = pin;
+			this.phNumber = phNumber;
+			this.email=email;
+		}
+		
 
 	public static void main(String[] args) {
 		//Display Message. 
 		System.out.println("Welcome to Address Book Program");
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number of contacts to add: ");
-		int n = sc.nextInt();
 		
-		//Multiple contacts can be created
-		for(int i =0; i<n ; i++)
+		Scanner sc = new Scanner(System.in);
+		AddressBookMain[] person = new AddressBookMain[20]; //Multiple contacts can be created
+		String firstName;
+		String lastName;
+		String address ;
+		String city;
+		String state;
+		String pin ;
+		String phNumber, email;
+		
+		int k = 1;
+		
+		do
 		{
-			System.out.println("Enter your first name: ");
-			String firstName = sc.nextLine();
-			System.out.println("Enter your last name: ");
-			String lastName = sc.nextLine();
-			System.out.println("Enter your Address: ");
-			String address = sc.nextLine();
-			System.out.println("Enter your city: ");
-			String city = sc.nextLine();
-			System.out.println("Enter your state: ");
-			String state = sc.nextLine();
-			System.out.println("Enter your city PIN Code: ");
-			String pin = sc.nextLine();
-			System.out.println("Enter your contact number: ");
-			String phNumber = sc.nextLine();
+			System.out.println("Enter your choice\n 1) Add Contact \n 2) Edit existing Contact\n 3) Delete contact\n 4) Display Address Book\n 5) Exit"); //user selection
+			int choice=sc.nextInt();
+			switch(choice)
+			{
+			case 1: {
+				System.out.println("Add Contacts: ");
+				System.out.println("Enter your first name: ");
+				firstName = sc.nextLine();
+				System.out.println("Enter your last name: ");
+				lastName = sc.nextLine();
+				System.out.println("Enter your Address: ");
+				address = sc.nextLine();
+				System.out.println("Enter your city: ");
+				city = sc.nextLine();
+				System.out.println("Enter your state: ");
+				state = sc.nextLine();
+				System.out.println("Enter your city PIN Code: ");
+				pin = sc.nextLine();
+				System.out.println("Enter your contact number: ");
+				phNumber = sc.nextLine();
+				System.out.println("Enter your Email ID: ");
+				email = sc.nextLine();
+				person[n]=new AddressBookMain(firstName,lastName,address,city,state,pin,phNumber,email);	//object creation
+				n++;
+				System.out.println("Details Added. \n");
+				//person[n].Display();		
+				break;
+			} //end of case 1
+			case 2:{
+				//Editing contacts - UC3
+				System.out.println("Enter the person's First Name: ");
+				String editFirstName = sc.nextLine();
+				System.out.println("Enter the person's Last Name: ");
+				String editLastName = sc.nextLine();
+				for (int j=0;j<n;j++)
+				{
+					if(person[j].firstName.equals(editFirstName) && person[j].lastName.equals(editLastName))
+					{
+						int edit1=j;
+						System.out.println("Enter your first name: ");
+						firstName = sc.nextLine();
+						System.out.println("Enter your last name: ");
+						lastName = sc.nextLine();
+						System.out.println("Enter your Address: ");
+						address = sc.nextLine();
+						System.out.println("Enter your city: ");
+						city = sc.nextLine();
+						System.out.println("Enter your state: ");
+						state = sc.nextLine();
+						System.out.println("Enter your city PIN Code: ");
+						pin = sc.nextLine();
+						System.out.println("Enter your contact number: ");
+						phNumber = sc.nextLine();
+						System.out.println("Enter your Email ID: ");
+						email = sc.nextLine();
+						person[edit1].editContact(firstName,lastName,address,city,state,pin,phNumber,email);  //calling Edit Contact
+					}// end of if
+				}// end of for
+				break;
+			}// end of case 2
+			case 3:{
+				//Deleting a contact UC4
+				break;
+			}
+			case 4: {
+				System.out.println("Contacts in Address Book: ");
+				for(int j=0; j<n ;j++)
+				{
+					System.out.println("Contact details of person: "+ (j+1));
+					person[j].Display();
+				}
+				break;
+			} //end of case 4
+			case 5: break;
+			default: 
+				System.out.println("Enter Valid Option");
+			}// end of switch
 			
-			//Creation of Object - UC2
-			AddressBookMain person = new AddressBookMain(firstName, lastName, address, city, state, pin, phNumber);
-			
-			//Displaying the details
-			System.out.println("Details Added. \n");
-			System.out.println("Name: " + person.firstName + " " + person.lastName);
-			System.out.println("Address: " + person.address + ", " + person.city + ", "+ person.state + " - " + person.pin);
-			System.out.println("Contact Number: " + person.phNumber + "\n");
-						
-		}// end of for
+		}while(k!=4);
 		sc.close();
 	}
 
