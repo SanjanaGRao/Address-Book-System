fff985dc71ae5190d498395f42b7fc6d71bb98a2
Sanjana Rao <sanjanagrao99@gmail.com>
2021-09-15 12:58:16 +0530

    Added: A method Duplicate name check to check if the contact already exits while entering

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -43,21 +43,24 @@ public class AddressBookMain {
 			System.out.println("Add A Contact: ");
 			System.out.println("Enter your first name: ");
 			firstName = sc.nextLine();
-			System.out.println("Enter your last name: ");
-			lastName = sc.nextLine();
-			System.out.println("Enter your Address: ");
-			address = sc.nextLine();
-			System.out.println("Enter your city: ");
-			city = sc.nextLine();
-			System.out.println("Enter your state: ");
-			state = sc.nextLine();
-			System.out.println("Enter your city PIN Code: ");
-			pin = sc.nextLine();
-			System.out.println("Enter your contact number: ");
-			phNumber = sc.nextLine();
-			System.out.println("Enter your Email ID: ");
-			email = sc.nextLine();
-			person1.add(new AddressBookMain(firstName,lastName,address,city,state,pin,phNumber,email));//object creation
+			if(duplicateCheck(firstName) == false)
+			{
+				System.out.println("Enter your last name: ");
+				lastName = sc.nextLine();
+				System.out.println("Enter your Address: ");
+				address = sc.nextLine();
+				System.out.println("Enter your city: ");
+				city = sc.nextLine();
+				System.out.println("Enter your state: ");
+				state = sc.nextLine();
+				System.out.println("Enter your city PIN Code: ");
+				pin = sc.nextLine();
+				System.out.println("Enter your contact number: ");
+				phNumber = sc.nextLine();
+				System.out.println("Enter your Email ID: ");
+				email = sc.nextLine();
+				person1.add(new AddressBookMain(firstName,lastName,address,city,state,pin,phNumber,email));//object creation
+			}
 			System.out.println("Details Added. \n");	
 		}
 		
@@ -75,28 +78,62 @@ public class AddressBookMain {
 				AddressBookMain contact = person1.get(j);
 				if(contact.firstName.equals(editFirstName) && contact.lastName.equals(editLastName))
 				{
-					System.out.println("Enter your first name: ");
-					firstName = sc.nextLine();
-					System.out.println("Enter your last name: ");
-					lastName = sc.nextLine();
-					System.out.println("Enter your Address: ");
-					address = sc.nextLine();
-					System.out.println("Enter your city: ");
-					city = sc.nextLine();
-					System.out.println("Enter your state: ");
-					state = sc.nextLine();
-					System.out.println("Enter your city PIN Code: ");
-					pin = sc.nextLine();
-					System.out.println("Enter your contact number: ");
-					phNumber = sc.nextLine();
-					System.out.println("Enter your Email ID: ");
-					email = sc.nextLine();
+					contact.displayAddressBook();
+					System.out.println("Enter the number of the detail you want to edit \n1.First Name\n2.Last Name\n3.Address\n4.City\n"
+							+ "5.State\n6.PIN Code\n7.Contact Number\n8.Email ID");
+	                int choose = sc.nextInt();
+	                switch(choose)
+	                {
+	                case 1: {
+	                	System.out.println("Enter your first name: ");
+	                	contact.firstName = sc.nextLine();
+	                	break;
+	                	}
+	                case 2: {
+	                	System.out.println("Enter your last name: ");
+						contact.lastName = sc.nextLine();
+						break;
+	                }
+	                case 3: {
+	                	System.out.println("Enter your Address: ");
+						contact.address = sc.nextLine();
+						break;
+	                }
+	                case 4: {
+	                	System.out.println("Enter your city: ");
+						contact.city = sc.nextLine();
+						break;
+	                }
+	                case 5: {
+	                	System.out.println("Enter your state: ");
+						contact.state = sc.nextLine();
+						break;
+	                }
+	                case 6: {
+	                	System.out.println("Enter your city PIN Code: ");
+						contact.pin = sc.nextLine();
+						break;
+	                }
+	                case 7: {
+	                	System.out.println("Enter your contact number: ");
+						contact.phNumber = sc.nextLine();
+						break;
+	                }
+	                case 8: {	
+						System.out.println("Enter your Email ID: ");
+						email = sc.nextLine();
+						break;
+	                }
+	                default: System.out.println("Invalid.");
+	               }
 				}// end of if
+				else
+					System.out.println("Contact not found.");
 			}// end of for
 		}
 		
 		//Function to delete a contact-UC4
-		public static void deleteContact()
+		public void deleteContact()
 		{
 			//Deleting a contact UC4
 			System.out.println("Enter the person's First Name: ");
@@ -111,6 +148,8 @@ public class AddressBookMain {
 					person1.remove(contact);	//Using built in method to delete an object
 					System.out.println("Contact Deleted.");
 				}
+				else
+					System.out.println("Contact not found.");
 			}
 			
 		}
@@ -129,6 +168,21 @@ public class AddressBookMain {
 					System.out.println("Contact Number: " + object.phNumber);
 					System.out.println("Email ID: " + object.email + "\n");
 				}
+		}
+		
+		//Method to check for duplicate contacts
+		public Boolean duplicateCheck(String name)
+		{
+		    for (int j=0; j<person1.size(); j++)
+	        {   
+	            AddressBookMain object = person1.get(j);
+	            if(object.firstName.equals(name))
+	            {
+	                System.out.println("Contact already exists in this Address Book."); 
+	                return true;
+	            }
+	        }
+		    return false;
 		}
 
 }
--- MultipleAddressBook.java
+++ MultipleAddressBook.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class MultipleAddressBook {
 
-	public static  int k=0;//to iterate the program many times
-	public static Scanner sc=new Scanner(System.in);
+	public static  int k = 0;//to iterate the program many times
+	public static Scanner sc = new Scanner(System.in);
 	
 	/*Method to perform different operations on contacts
 	 * @param 'choice' refers to user's input i.e.,the operations the user wants to perform on addressbook
@@ -26,21 +26,24 @@ public class MultipleAddressBook {
 			case 5: person.displayAddressBook();//To display whole addressbook
 					break;	
 					
+			default: System.out.println("Invalid option"); 
+					 break;
+					
 			}
 	}
 			
 			public static void main(String[] args) {
 
-				Hashtable<String,AddressBookMain>  multipleAddressBook = new Hashtable<String, AddressBookMain>();  //dictionary for mapping different address book with key 
+				Hashtable<String,AddressBookMain>  multipleAddressBook = new Hashtable<>();  //dictionary for mapping different address book with key 
 				AddressBookMain person = new AddressBookMain();					//first addressbook
 				
 				String n="";
 				String y= "AddressBook1" ;
-				multipleAddressBook.put(y, person);							//put the first addressbook into dictionary
+				multipleAddressBook.put(y, person);	//put the first addressbook into dictionary
 				
-				while(k!=6) //it repeats till the user wants to end
+				while(k!=7) //it repeats till the user wants to end
 				{
-					System.out.println("Enter your choice\n 1) Create New Address Book\n 2) Add Contact \n 3) Edit existing Contact\n 4) Delete contact\n 5) Display Address Book\n 6) Exit"); //user selection
+					System.out.println("Enter your choice\n 1) Create New Address Book\n 2) Add Contact \n 3) Edit existing Contact\n 4) Delete contact\n 5) Display Address Book\n 6) Create Another AddressBook 7) Exit"); //user selection
 					int choice=sc.nextInt();
 					if(choice==1)  //to create a new address book
 					{
@@ -49,6 +52,18 @@ public class MultipleAddressBook {
 						AddressBookMain addressBook1 = new AddressBookMain();  // a new addressbook object is created 
 						multipleAddressBook.put(n, addressBook1);
 							
+					}
+					if(choice==5)  
+					{
+						System.out.println("Enter the name of the Address Book: ");
+						n = sc.next();
+						if(multipleAddressBook.containsKey(n))
+						{
+						    System.out.println("Address Book already exists.");
+						    n=sc.next();
+						}
+						AddressBookMain addressBook2 = new AddressBookMain();  
+						multipleAddressBook.put(n, addressBook2);							
 					}
 					else
 					{
